fdd5924b8f7638749c475a73eb21bb9cfd0433e1
Ivan <ostankov-ii@mail.ru>
2019-12-07 19:23:47 +0300

    egerg

---
+++ server.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<form action="/test" method="post">
+<input type="text" name="txt">
+<input type="submit" value="sent">
+    
+</body>
+</html>
